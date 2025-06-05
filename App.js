da500a644ec89a209208f93a870d003b9e1614b1
saumyapaudel <paudelsaumya@gmail.com>
2025-06-05 05:29:44 -0400

    Upload to Git

--- App.js
+++ App.js
@@ -1,11 +1,58 @@
+import React, {useState} from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Platform, StyleSheet, Text, Touchable, TouchableOpacity, View } from 'react-native';
+import TaskItem from './components/TaskItem';
+import { KeyboardAvoidingView, TextInput, Keyboard } from 'react-native';
 
 export default function App() {
+  const [taskItem, setTaskItem] = useState();
+  const [taskItemsArr, setTaskItemsArr] = useState([]);
+
+  const addTask = () => {
+    if(taskItem != ''){
+      setTaskItemsArr([...taskItemsArr, taskItem]);
+      setTaskItem('');
+      Keyboard.dismiss();
+    }
+  }
+  
+  const deleteTask = (index) => {
+    const copy = [...taskItemsArr];
+    copy.splice(index, 1);
+    setTaskItemsArr(copy);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+        <View style={styles.wrapper}>
+          <Text style={styles.title}>SIMPLE TASK MANAGER</Text>
+
+          <View style={styles.tasks}>
+            {
+              taskItemsArr.map((item, index) => (
+                <TaskItem 
+                  key={index} 
+                  text={item} 
+                  index={index} 
+                  deleteTask={deleteTask} 
+                />
+              ))
+            }
+          </View>
+
+        </View>
+    
+    <KeyboardAvoidingView 
+    behavior = {Platform.OS === 'ios' ? 'padding' : 'height'}
+    style={styles.inputTaskWrapper}
+    >
+      <TextInput style={styles.taskInput} placeholder= 'E.g.: Do Laundry, Walk the Dog, etc...' onChangeText={text => setTaskItem(text)} value={taskItem}/>
+      <TouchableOpacity onPress={() => addTask()}>
+        <View style= {styles.addTaskWrapper}>
+          <Text style = {styles.addIcon}> + </Text>
+        </View>
+      </TouchableOpacity>
+    </KeyboardAvoidingView>
     </View>
   );
 }
@@ -13,8 +60,54 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#f9f7f2',
+
   },
+  wrapper: {
+    paddingTop: 60,
+    paddingHorizontal: 20,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    color: '	#1e2a38',
+  },
+  tasks: {
+    marginTop: 20,
+  },
+  inputTaskWrapper: {
+    position: 'absolute',
+    width: '100%',
+    bottom: 60,
+    paddingHorizontal: 20, 
+    paddingVertical: 15, 
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    backgroundColor: '#f9f7f2', 
+  },
+  taskInput: {
+    flex: 1, 
+    marginRight: 15,
+    paddingVertical: 10,
+    paddingHorizontal: 15,
+    backgroundColor: '#fff',
+    borderRadius: 10,
+    borderColor: '#ccc',
+    borderWidth: 1,
+  },
+  addTaskWrapper: {
+    width: 50,
+    height: 50,
+    backgroundColor: '#6bb89e',
+    borderRadius: 25,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  addIcon: {
+    color: '#fff',
+    fontSize: 24,
+  },
+            
 });
---
+++ components/TaskItem.js
@@ -0,0 +1,59 @@
+import React, {useState} from "react";
+import {View, Text, StyleSheet, TouchableOpacity} from 'react-native';
+import CheckBox from "expo-checkbox";
+
+const TaskItem = (props) => {
+    const [markAsComplete, setMarkedAsComplete] = useState(false);
+    return (
+        <View style ={styles.taskItem}>
+            <CheckBox style = {styles.checkbox} value={markAsComplete} onValueChange={setMarkedAsComplete} />
+            <Text style = {[styles.description, markAsComplete&& {
+                textDecorationLine: 'line-through',
+                color: '#333333',
+                opacity: 0.6}
+        ]}>{props.text}</Text>
+        <TouchableOpacity onPress={() => props.deleteTask(props.index)}>
+            <Text style={styles.delete}>Remove</Text>
+        </TouchableOpacity>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    taskItem: {
+        backgroundColor: '#a8e6cf',
+        padding: 10,
+        borderRadius: 20,
+        margin: 10,
+        flexDirection: "row",
+        alignContent: 'center',
+
+    },
+    checkbox: {
+        width: 24,
+        height: 24,
+        borderRadius: 15,
+        marginRight: 10,
+
+    },
+    description: {
+        flex: 1,
+        maxWidth: '80%',
+        fontWeight: '500',
+        fontSize: 16,
+    },
+    delete: {
+        paddingHorizontal: 6,
+        paddingVertical: 1,
+        borderRadius: 15,
+        borderColor: "#CD5C5C",
+        borderWidth: 4,
+        fontSize: 12,
+        color: "#CD5C5C",
+        fontWeight: "bold",
+        textAlign: "center",
+        alignContent: 'center'
+    },
+})
+
+export default TaskItem;
